893cb1aa2e0bace7795e4a052b1e52defa0f8572
Yibeltal Aseffa <yibeltalnetsanet2006@gmail.com>
2025-02-20 17:23:52 -0500

    Initial commit for portfolio1

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/portifolio/", // Ensure this matches your repo name
+  base: "/yibe-portfolio/", // Ensure this matches your repo name
 });
